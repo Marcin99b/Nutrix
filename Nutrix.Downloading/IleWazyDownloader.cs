97716012441d0df417fe38edd1c36c76740f146d
Marcin99b <marcin.bigos99@gmail.com>
2025-02-01 21:05:02 +0100

    Add basic importer and downloader

---
+++ Nutrix.Downloading/IleWazyDownloader.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nutrix.Downloader;
+internal class IleWazyDownloader
+{
+    public void Download()
+    {
+
+        var resultsPath = "C:/Users/marci/Desktop/Nutrix/Downloader/Nutrix.Downloader/Results";
+        var client = new HttpClient();
+        var html = new HtmlDocument();
+
+        var lastPage = Directory.GetFiles(resultsPath)
+            .Select(Path.GetFileName)
+            .Select(x => x!.Split('_')[0])
+            .Select(int.Parse)
+            .OrderByDescending(x => x)
+            .FirstOrDefault(1);
+
+        await DownloadPage(lastPage);
+
+        async Task DownloadPage(int page)
+        {
+            var productsOnPage = await GetUrlsToProductsOnPage(page);
+            if (productsOnPage.Length == 0)
+            {
+                return;
+            }
+
+            foreach (var productUrl in productsOnPage)
+            {
+                Log($"Waiting 200ms before download product");
+                await Task.Delay(200);
+                var name = productUrl.Replace("http://www.ilewazy.pl/", string.Empty);
+                var sw = Stopwatch.StartNew();
+                var content = await DownloadString(productUrl);
+                sw.Stop();
+                Log($"Downloaded product {name} in time {sw.ElapsedMilliseconds}ms");
+                var fileName = $"{page}_{name}.html";
+                File.WriteAllText(Path.Combine(resultsPath!, fileName), content);
+                Log($"Saved file {fileName} with length {content.Length}");
+            }
+
+            var totalProducts = Directory.GetFiles(resultsPath).Length;
+            Log($"Total downloaded products: {totalProducts}");
+            Log("===========================================");
+            await DownloadPage(page + 1);
+        }
+    }
+}
+
+
+async Task<string[]> GetUrlsToProductsOnPage(int page)
+{
+    var url = GetPageUrl(page);
+
+    var sw = Stopwatch.StartNew();
+    var content = await DownloadString(url);
+
+    if (content.Contains("Niestety nie udało nam się nic dla Ciebie znaleźć..."))
+    {
+        Log($"Page {page} not found. Last existing is {page - 1}");
+        return Array.Empty<string>();
+    }
+
+    sw.Stop();
+    Log($"Downloaded page {page} in time {sw.ElapsedMilliseconds}ms");
+
+    html!.LoadHtml(content);
+
+    var items = html.DocumentNode.SelectNodes("//li[contains(@class, 'span3')]/div[contains(@class, 'subtitle')]/a")
+        .Select(x => x.Attributes.First(x => x.Name == "href").Value)
+        .ToArray();
+
+    Log($"Found {items.Length} products on page {page}");
+    return items;
+}
+
+string GetPageUrl(int id) => $"http://www.ilewazy.pl/produkty/page/{id}/s/date/k/asc/";
+
+void Log(string text) => Console.WriteLine($"{DateTime.Now:HH-mm-ss}: {text}");
+
+async Task<string> DownloadString(string url)
+{
+    var normalizedUrl = RemoveDiacritics(url);
+    return await client!.GetStringAsync(normalizedUrl);
+}
+
+string RemoveDiacritics(string text)
+{
+    if (string.IsNullOrEmpty(text))
+    {
+        return text;
+    }
+
+    var normalizedString = text.Normalize(NormalizationForm.FormD);
+    var stringBuilder = new StringBuilder();
+
+    foreach (char c in normalizedString)
+    {
+        if (CharUnicodeInfo.GetUnicodeCategory(c) != UnicodeCategory.NonSpacingMark)
+            stringBuilder.Append(c);
+    }
+
+    return stringBuilder.ToString().Normalize(NormalizationForm.FormC);
+}
---
+++ Nutrix.Importing/IleWazyImporter.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nutrix.Importing;
+internal class IleWazyImporter
+{
+    public void Import()
+    {
+
+        var resultsPath = @"C:/Users/marci/Desktop/Nutrix/Downloader/Nutrix.Downloader/Results";
+        string[] files = Directory.GetFiles(resultsPath);
+
+        var products = new ConcurrentBag<Product>();
+        Parallel.ForEach(files, file => Process(File.ReadAllText(file)));
+        await InsertToDatabase();
+
+        void Process(string content)
+        {
+            var html = new HtmlDocument();
+            html.LoadHtml(content);
+            var name = html.DocumentNode.SelectSingleNode("//h1[contains(@class, 'weighting-title')]").InnerText.Trim();
+            var table = html.DocumentNode.SelectSingleNode("//table[@id='ilewazy-ingedients']");
+            html.LoadHtml(table.InnerHtml);
+
+            var values = table
+            .SelectNodes("//tbody/tr")
+            .Select(x =>
+            {
+                try
+                {
+                    var text = x.ChildNodes[3].InnerText;
+                    var trimmed = text.Trim(" ,gkcal\r\n\\b.d.".ToCharArray());
+                    if (string.IsNullOrWhiteSpace(trimmed))
+                    {
+                        trimmed = "0";
+                    }
+
+                    var dec100g = decimal.Parse(trimmed);
+                    var per1000g = Convert.ToInt32(dec100g * 10);
+                    return new
+                    {
+                        Name = x.ChildNodes[1].InnerText,
+                        Value = per1000g
+                    };
+                }
+                catch (Exception ex)
+                {
+                    throw;
+                }
+
+            }).ToArray();
+
+            var product = new Product(
+                name,
+                values.First(x => x.Name.Contains("Energia")).Value,
+                values.First(x => x.Name.Contains("Białko")).Value,
+                values.First(x => x.Name.Contains("Tłuszcz")).Value,
+                values.First(x => x.Name.Contains("Węglowodany")).Value,
+                values.First(x => x.Name.Contains("Błonnik")).Value);
+
+            products!.Add(product);
+        }
+    }
+
+    async Task InsertToDatabase()
+    {
+        var connectionString = "Host=localhost;Username=postgres;Database=postgres";
+        await using var dataSource = NpgsqlDataSource.Create(connectionString);
+
+        foreach (var product in products)
+        {
+            await using (var cmd = dataSource.CreateCommand(
+                @"INSERT INTO food_products (name, calories_1000g, proteins_1000g, fats_1000g, carbs_1000g, fiber_1000g)
+        VALUES (($1), ($2), ($3), ($4), ($5), ($6))"))
+            {
+                cmd.Parameters.AddWithValue(product.Title);
+                cmd.Parameters.AddWithValue(product.KcalPer1000g);
+                cmd.Parameters.AddWithValue(product.ProteinPer1000g);
+                cmd.Parameters.AddWithValue(product.FatPer1000g);
+                cmd.Parameters.AddWithValue(product.CarbsPer1000g);
+                cmd.Parameters.AddWithValue(product.FiberPer1000g);
+
+                await cmd.ExecuteNonQueryAsync();
+            }
+        }
+
+        products.Clear();
+    }
+
+    record Product(string Title, int KcalPer1000g, int ProteinPer1000g, int FatPer1000g, int CarbsPer1000g, int FiberPer1000g);
+}
