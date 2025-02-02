28433934b411005db1cd56bff808ca2130375a53
Marcin99b <marcin.bigos99@gmail.com>
2025-02-02 21:15:20 +0100

    downloader refactoring

---
+++ Nutrix.Commons/FileSystem/NutrixPaths.cs
@@ -0,0 +1,6 @@
+﻿namespace Nutrix.Commons.FileSystem;
+public static class NutrixPaths
+{
+    public static string GetDownloaderResult(string downloaderName)
+        => Path.Combine(Directory.GetCurrentDirectory(), "DownloadResults", downloaderName);
+}
--- Nutrix.Downloading/IleWazyDownloader.cs
+++ Nutrix.Downloading/IleWazyDownloader.cs
@@ -1,20 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using HtmlAgilityPack;
+using Nutrix.Commons.FileSystem;
+using System;
 using System.Diagnostics;
 using System.Globalization;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Nutrix.Downloader;
 internal class IleWazyDownloader
 {
-    public void Download()
+    private readonly HttpClient client = new();
+    private readonly string resultsPath = NutrixPaths.GetDownloaderResult(nameof(IleWazyDownloader));
+
+    public IleWazyDownloader()
     {
+        if (!Directory.Exists(resultsPath))
+        {
+            Directory.CreateDirectory(resultsPath);
+        }
+    }
 
-        var client = new HttpClient();
-        var html = new HtmlDocument();
-
+    public async Task Download()
+    {
         var lastPage = Directory.GetFiles(resultsPath)
             .Select(Path.GetFileName)
             .Select(x => x!.Split('_')[0])
@@ -22,90 +28,82 @@ internal class IleWazyDownloader
             .OrderByDescending(x => x)
             .FirstOrDefault(1);
 
-        await DownloadPage(lastPage);
-
-        async Task DownloadPage(int page)
+        var morePages = false;
+        do
         {
-            var productsOnPage = await GetUrlsToProductsOnPage(page);
-            if (productsOnPage.Length == 0)
+            morePages = await this.DownloadPage(lastPage);
+            if (morePages)
             {
-                return;
+                lastPage++;
             }
+        } while (morePages);
+    }
 
-            foreach (var productUrl in productsOnPage)
-            {
-                Log($"Waiting 200ms before download product");
-                await Task.Delay(200);
-                var name = productUrl.Replace("http://www.ilewazy.pl/", string.Empty);
-                var sw = Stopwatch.StartNew();
-                var content = await DownloadString(productUrl);
-                sw.Stop();
-                Log($"Downloaded product {name} in time {sw.ElapsedMilliseconds}ms");
-                var fileName = $"{page}_{name}.html";
-                File.WriteAllText(Path.Combine(resultsPath!, fileName), content);
-                Log($"Saved file {fileName} with length {content.Length}");
-            }
-
-            var totalProducts = Directory.GetFiles(resultsPath).Length;
-            Log($"Total downloaded products: {totalProducts}");
-            Log("===========================================");
-            await DownloadPage(page + 1);
+    async Task<bool> DownloadPage(int page)
+    {
+        var productsOnPage = await this.GetUrlsToProductsOnPage(page);
+        if (productsOnPage.Length == 0)
+        {
+            return false;
         }
+
+        foreach (var productUrl in productsOnPage)
+        {
+            await Task.Delay(200);
+            var name = productUrl.Replace("http://www.ilewazy.pl/", string.Empty);
+
+            var sw = Stopwatch.StartNew();
+            var content = await client!.GetStringAsync(productUrl);
+            sw.Stop();
+
+            var fileName = $"{page}_{name}.html";
+            File.WriteAllText(Path.Combine(resultsPath!, fileName), content);
+        }
+
+        var totalProducts = Directory.GetFiles(resultsPath).Length;
+        return totalProducts > 0;
     }
-}
 
-
-async Task<string[]> GetUrlsToProductsOnPage(int page)
-{
-    var url = GetPageUrl(page);
-
-    var sw = Stopwatch.StartNew();
-    var content = await DownloadString(url);
-
-    if (content.Contains("Niestety nie udało nam się nic dla Ciebie znaleźć..."))
+    async Task<string[]> GetUrlsToProductsOnPage(int page)
     {
-        Log($"Page {page} not found. Last existing is {page - 1}");
-        return Array.Empty<string>();
+        var url = this.GetPageUrl(page);
+
+        var content = await client!.GetStringAsync(url);
+        if (content.Contains("Niestety nie udało nam się nic dla Ciebie znaleźć..."))
+        {
+            return Array.Empty<string>();
+        }
+
+        var html = new HtmlDocument();
+        html!.LoadHtml(content);
+        var items = html.DocumentNode.SelectNodes("//li[contains(@class, 'span3')]/div[contains(@class, 'subtitle')]/a")
+            .Select(x => x.Attributes.First(x => x.Name == "href").Value)
+            .Select(this.RemoveDiacritics)
+            .ToArray();
+
+        return items;
     }
 
-    sw.Stop();
-    Log($"Downloaded page {page} in time {sw.ElapsedMilliseconds}ms");
+    string GetPageUrl(int id) => this.RemoveDiacritics($"http://www.ilewazy.pl/produkty/page/{id}/s/date/k/asc/");
 
-    html!.LoadHtml(content);
-
-    var items = html.DocumentNode.SelectNodes("//li[contains(@class, 'span3')]/div[contains(@class, 'subtitle')]/a")
-        .Select(x => x.Attributes.First(x => x.Name == "href").Value)
-        .ToArray();
-
-    Log($"Found {items.Length} products on page {page}");
-    return items;
-}
-
-string GetPageUrl(int id) => $"http://www.ilewazy.pl/produkty/page/{id}/s/date/k/asc/";
-
-void Log(string text) => Console.WriteLine($"{DateTime.Now:HH-mm-ss}: {text}");
-
-async Task<string> DownloadString(string url)
-{
-    var normalizedUrl = RemoveDiacritics(url);
-    return await client!.GetStringAsync(normalizedUrl);
-}
-
-string RemoveDiacritics(string text)
-{
-    if (string.IsNullOrEmpty(text))
+    private string RemoveDiacritics(string text)
     {
-        return text;
+        if (string.IsNullOrEmpty(text))
+        {
+            return text;
+        }
+
+        var normalizedString = text.Normalize(NormalizationForm.FormD);
+        var stringBuilder = new StringBuilder();
+
+        foreach (char c in normalizedString)
+        {
+            if (CharUnicodeInfo.GetUnicodeCategory(c) != UnicodeCategory.NonSpacingMark)
+            {
+                stringBuilder.Append(c);
+            }
+        }
+
+        return stringBuilder.ToString().Normalize(NormalizationForm.FormC);
     }
-
-    var normalizedString = text.Normalize(NormalizationForm.FormD);
-    var stringBuilder = new StringBuilder();
-
-    foreach (char c in normalizedString)
-    {
-        if (CharUnicodeInfo.GetUnicodeCategory(c) != UnicodeCategory.NonSpacingMark)
-            stringBuilder.Append(c);
-    }
-
-    return stringBuilder.ToString().Normalize(NormalizationForm.FormC);
 }
