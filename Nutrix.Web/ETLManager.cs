8636cd68e856c322a4bc200d6920b4e6a963c289
Marcin99b <marcin.bigos99@gmail.com>
2025-02-04 12:46:25 +0100

    remove context

--- Nutrix.Web/ETLManager.cs
+++ Nutrix.Web/ETLManager.cs
@@ -1,22 +1,15 @@
 ﻿using Nutrix.Downloading;
 using Nutrix.Importing;
-using Serilog.Context;
 
 public class ETLManager(IleWazyDownloader ileWazyDownloader, IleWazyImporter ileWazyImporter)
 {
     public async Task RunDownloader(string downloader)
     {
-        using (LogContext.PushProperty("RunDownloader_CorrelationId", Guid.NewGuid().ToString()))
-        {
-            await ileWazyDownloader.Download();
-        }
+        await ileWazyDownloader.Download();
     }
 
     public async Task RunImporter(string importer)
     {
-        using (LogContext.PushProperty("RunImporter_CorrelationId", Guid.NewGuid().ToString()))
-        {
-            await ileWazyImporter.Import();
-        }
+        await ileWazyImporter.Import();
     }
 }
